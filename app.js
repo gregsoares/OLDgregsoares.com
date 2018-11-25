d81ef7efe7d2eaffe6921aca7662fa1041ddeace
gregsoares <gregasoares@gmail.com>
2018-11-25 02:04:20 -0500

    Rnders custom error page.

--- app.js
+++ app.js
@@ -2,6 +2,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
 // Declaring Routers
 const indexRouter = require('./routes/index');
@@ -12,23 +14,60 @@ const apiRouter = require('./routes/api');
 const app = express();
 
 // Setting up path, views and engine
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.set('views', path.join(__dirname, '/views'));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../', 'views')))
 app.set('view engine', 'jsx');
-app.engine('jsx', require('express-react-views').createEngine());
+const options = ({
+  transform: {},
+});
+app.engine('jsx', require('express-react-views').createEngine(options));
 
 // Setting up routes
-app.get('/error/:', errorRouter);
+app.use('/', indexRouter);
+app.use('/error/:', errorRouter);
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 app.use('/api/:id', apiRouter);
-app.use('/', indexRouter);
 
 // MongoDB Keys and Connection info
 // const db = require('../config/keys').prodURI;
 // mongoose.connect(db, { useNewUrlParser: true })
 //   .then(() => console.log('MongoDB connected.'))
 //   .catch(err => console.error(err));
+
+
+// @Error handlers
+// @404 error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+
+// @development error handler
+// Print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// @production error handler
+// Hide stacktrace from user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
 
 module.exports = app;
--- routes/error.js
+++ routes/error.js
@@ -3,13 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 /* 404 Error */
-router.get('/error/404', (req, res) => {
-  res.render('/Error404/index', { title: 'Much like the squirrel, I forgot where I put my ...data', Error: 'error' });
+router.get('/404', (req, res) => {
+  res.render('/error/index', { title: 'Much like the squirrel, I forgot where I put my ...data', Error: 'error' });
 });
 
 // error.capturestacktrace(error);
 router.get('/error/500', (req, res) => {
-  res.sendStatus(300);
+  res.sendStatus(200);
 });
 
 module.exports = router;
