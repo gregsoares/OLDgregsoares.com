0fa426d7e1614602b3d74477fba64af7b2a4b0e7
gregsoares <gregasoares@gmail.com>
2019-01-13 20:43:09 -0500

    Added: "engines"  block on package.json

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,6 @@ const router = express.Router();
 // @route GET /
 // @desc handles any GET request to index
 // @access Public
-router.get('/', (req, res) => res.json({ msg: 'Hello World'}));
+router.get('/', (req, res) => res.json({ msg: 'Hello World - Page Coming Soon'}));
 
 module.exports = router;
