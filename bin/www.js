bfe69190cf3a7264f022d989b69ea045eb5b0abf
gregsoares <gregasoares@gmail.com>
2019-04-10 14:57:41 -0400

    +'../' - www.js for path traversal to static files

--- bin/www.js
+++ bin/www.js
@@ -7,10 +7,10 @@ const port = process.env.PORT || 3001;
 
 // If in production, serve static file and all subsequent files from prod. env.
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static('../client/build'));
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
 
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, '..', 'client', 'build', 'index.html'));
   });
 }
 
