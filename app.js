a1c20f69991cc8e7aa7cd8d833983afb42e449a9
gregsoares <gregasoares@gmail.com>
2019-04-10 13:35:30 -0400

    Heroku Deployement Fix #5

--- app.js
+++ app.js
@@ -6,10 +6,9 @@ const app = express();
 let db;
 
 //App Setup
-app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'public')));
 
 //Import DB keys and config
 // const db = require('./config/keys').devURI;
