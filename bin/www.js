381de15f003884160516e2f470979de306a5e15d
gregsoares <gregasoares@gmail.com>
2019-04-10 14:42:55 -0400

    fixed www.js path to build for production mode

--- bin/www.js
+++ bin/www.js
@@ -1,15 +1,14 @@
 //Server file
 const app = require('../app');
-const path = require('path');
 const port = process.env.PORT || 3001;
 
 // If in production, serve static file and all subsequent files from prod. env.
 if(process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
+  app.use(express.static('../client/build'));
 
   app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
   });
 }
 
-app.listen(port, () => console.log(`Server Listening on port ${port}`));
+app.listen(port, () => console.log(`Express server listening on port ${port} in ${app.settings.env} mode`));
