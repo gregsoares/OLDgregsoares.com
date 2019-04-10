aae01d3a1f58e3f3a256d6e68dd82454c4f45a9d
gregsoares <gregasoares@gmail.com>
2019-04-10 13:17:04 -0400

    cleanup + production check for deployment build

--- bin/www.js
+++ bin/www.js
@@ -4,12 +4,12 @@ const app = require('../app');
 const port = process.env.PORT || 3001;
 
 // If in production, serve static file and all subsequent files from prod. env.
-// if(process.env.NODE_ENV === 'production') {
-//   app.use(express.static('public/build'));
+if(process.env.NODE_ENV === 'production') {
+  app.use(express.static('public/build'));
 
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html'));
-//   });
-// }
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'public', 'build', 'index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Server Listening on port ${port}`));
