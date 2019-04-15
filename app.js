12ce5589863e8bb329dba761d6849b3e57169e9b
gregsoares <gregasoares@gmail.com>
2019-04-14 20:15:23 -0400

    Bck to the right file for prod check.

--- app.js
+++ app.js
@@ -5,6 +5,18 @@ const path = require('path');
 const app = express();
 let db;
 
+// If it's in production mode
+if(process.env.NODE_ENV === 'production') {
+app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+});
+} else {
+  app.use(express.static(path.join(__dirname, '..', 'client', 'src')));
+}
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
