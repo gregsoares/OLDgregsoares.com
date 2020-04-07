bb103039869626e454ea8287fd80da08223ee496
Greg Soares <gregselvagem@gmail.com>
2020-04-06 20:10:08 -0400

    +tailwindcss = setup, tested

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 import React, { useState, useCallback } from 'react';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
+import './Styles/app.css';
 // Context
-// import { TopNavState } from "./Store/Context";
 
 // Temp GeneralHeaders import
 
