8226316b5673b61715993a34c727d96dca4679a6
Greg <gregselvagem@gmail.com>
2020-06-29 01:21:41 -0400

    +First project(Reactify)

--- client/src/Components/FeaturedSection/FeaturedSection.js
+++ client/src/Components/FeaturedSection/FeaturedSection.js
@@ -3,14 +3,41 @@ import React from 'react'
 import monitoringImg from './assets/monitoring.jpg';
 import reportingImg from './assets/reporting.jpg';
 import syncingImg from './assets/syncing.jpg';
+import AgencyImg from './assets/AgencySS.png';
 
 export const FeaturedSection = () => {
   return (
-    <div data-testid="FeaturedSectionContainer">
-      <section className="container p-10 px-6 mx-auto">
+    <div className="" data-testid="FeaturedSectionContainer">
+      <section className="">
         <h2 className="mb-8 text-4xl font-bold text-center text-gray-800">
-          Features
+          Projects
         </h2>
+        <div className="flex flex-wrap justify-around py-8 pb-10 overflow-hidden bg-gray-300 rounded-lg">
+          <div className="flex-col mx-auto text-center ">
+            <h4 className="pb-0 mb-0 text-3xl font-bold text-gray-800 ">"Reactify"</h4>
+            <p className="pt-0 mt-0 mb-5 text-gray-700">Full MERN Stack</p>
+            <div className="flex-col justify-center mb-5 text-gray-700 ">
+              <p className="my-3" >Static Bootstrap Template <span className="text-2xl"> ➡ </span> Full Stack Web Application</p>
+              <ul className="">
+                <li className="mb-1 leading-relaxed tracking-wide "><p className="ml-4 sm:ml-6 md:ml-10">
+                  Each section ready for export as stand-alone React Component
+                </p> </li>
+                <li className="mb-1 leading-relaxed tracking-wide "><p className="ml-4 sm:ml-6 md:ml-10">
+                  REST API ready
+                </p> </li>
+                <li className="mb-1 leading-relaxed tracking-wide "><p className="ml-4 sm:ml-6 md:ml-10">
+                  Bootstrap + FontAwesome
+                </p> </li>
+                <li className="mb-1 leading-relaxed tracking-wide "><p className="ml-4 sm:ml-6 md:ml-10">
+                  HTML, CSS3, JavaScript, ReactJs
+                </p> </li>
+              </ul>
+            </div>
+          </div>
+          <div className="w-full md:w-2/5 sm:px-4">
+            <img className="rounded-md" src={AgencyImg} alt="Monitoring" />
+          </div>
+        </div>
         <div className="flex flex-wrap items-center mb-20">
           <div className="w-full md:w-1/2">
             <h4 className="mb-3 text-3xl font-bold text-gray-800">Exercise Metric</h4>
--- client/src/Pages/Index.js
+++ client/src/Pages/Index.js
@@ -1,31 +1,18 @@
 import React from 'react';
 
-// import { Container } from "../Components/Container/Container";
-import { CalendarCard } from '../Components/CalendarCard/CalendarCard';
 import { CardSection } from '../Components/CardSection/CardSection';
-import { Tags } from '../Components/CardSection/Tags';
-// import { Github } from '../Components/Github/Github';
 import { FeaturedSection } from '../Components/FeaturedSection/FeaturedSection';
 import { HeroCTA } from '../Components/HeroCTA/HeroCTA';
-// import { Carousel } from '../Components/Carousel/Carousel';
-
 
 const Index = () => {
 
   return (
-      <main className="w-full h-full p-0 m-0" id="IndexPage" data-testid="IndexPageContainer">
-        <HeroCTA />
-        <FeaturedSection />
-        <CardSection />
+    <main className="w-full h-full p-0 m-0" id="IndexPage" data-testid="IndexPageContainer">
+      <HeroCTA />
+      <FeaturedSection />
+      <CardSection />
 
-        {/* <Carousel /> */}
-        {/* <Github /> */}
-        <CalendarCard />
-
-        <Tags text='OneTag' />
-
-        <Tags text={null} />
-      </main>
+    </main>
   )
 }
 
