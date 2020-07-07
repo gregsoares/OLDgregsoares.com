6d26e031c3b95b3cbde432c52acedd8ec26f56ef
Greg <gregselvagem@gmail.com>
2020-07-07 13:11:48 -0400

    pc-test commit #1

--- client/src/Pages/Projects.js
+++ client/src/Pages/Projects.js
@@ -2,11 +2,13 @@ import React from 'react'
 
 // Project Components import
 import { SampleReviewCard } from "../Components/Projects/Projects";
+import { TerminalComp } from "../Components/TerminalComp/TerminalComp";
 
  const Projects = () => {
   return (
     <div className="flex items-center justify-center h-screen bg-gray-600">
       <SampleReviewCard />
+      <TerminalComp />
     </div>
   )
 }
