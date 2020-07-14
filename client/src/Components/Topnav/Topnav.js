4e89e46cc2a3efe283b161b9c1faeddc3540f907
Greg <gregselvagem@gmail.com>
2020-07-13 21:46:16 -0400

    +github link in TopNav

--- client/src/Components/Topnav/Topnav.js
+++ client/src/Components/Topnav/Topnav.js
@@ -13,10 +13,10 @@ export const Topnav = props => {
         <p className="py-0 font-serif text-2xl  ">GregSoares.com</p>
       </div>
       <div className="flex justify-end w-full mr-4 py-0 navbar-menu font-semibold">
-        <a className={isActive("home") + ''} href="/" >Projects</a>
+        <a className={isActive("home") + ''} href="/" >Home</a>
         <a className={isActive("projects") + ''} href="/projects" >Projects</a>
-        <a className={isActive("resume") + ''} href="https://www.linkedin.com/in/greg-soares/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAABkED18BwLp2ur2OWi7pIobAZZSlpJBTgp0,1593197804619)/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BGRPVrhLgTa272qWWSqugbw%3D%3D&licu=urn%3Ali%3Acontrol%3Ad_flagship3_profile_view_base-featured_item_detail_view" >Resume</a>
-        <a className={isActive("github") + ''} href="#github" >Github</a>
+        <a className={isActive("resume") + ''} href="https://www.linkedin.com/in/greg-soares/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAABkED18BwLp2ur2OWi7pIobAZZSlpJBTgp0,1593197804619)/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BGRPVrhLgTa272qWWSqugbw%3D%3D&licu=urn%3Ali%3Acontrol%3Ad_flagship3_profile_view_base-featured_item_detail_view"  target="_blank">Resume</a>
+        <a className={isActive("github") + ''} href="https://github.com/gregsoares" target="_blank" >Github</a>
         <a className={isActive("linkedin") + ''} href="http://www.linkedin.com/in/greg-soares" target="_blank" >LinkedIn</a>
       </div>
     </nav>
