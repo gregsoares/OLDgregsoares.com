30462b988c0fc7c9aa2a1fbadbd44233306ff82f
Greg <gregselvagem@gmail.com>
2020-07-17 17:54:18 -0400

    -App.js bug: coverage import

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,27 @@
-import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 // Components Imports
 import { Topnav } from "./Components/Topnav/Topnav";
-import { Footer } from './Components/Footer/Footer';
+import { Footer } from "./Components/Footer/Footer";
 
 // Pages Imports
 import Index from "./Pages/Index";
 import About from "./Pages/About";
-import Projects from './Pages/Projects';
-import { Coverage } from "./coverage/lcov-report/Pages/Coverage";
+import Projects from "./Pages/Projects";
 
 const App = () => {
   return (
     <Router>
       <Topnav activePage />
       <Switch>
-        <Route exact path='/' component={Index} />
-        <Route exact path='/about' component={About} />
-        <Route exact path='/projects' component={Projects} />
-        <Route exact path='/supaSecretAdmin/coverage' component={Coverage} />
+        <Route exact path="/" component={Index} />
+        <Route exact path="/about" component={About} />
+        <Route exact path="/projects" component={Projects} />
       </Switch>
       <Footer />
     </Router>
   );
-}
+};
 
 export default App;
