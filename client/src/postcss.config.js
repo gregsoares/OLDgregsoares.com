7d190c7ca59932d193cd7137d39373bb2c3e6691
Greg <gregselvagem@gmail.com>
2020-08-17 22:46:05 -0400

    postcss purge config setup

--- client/src/postcss.config.js
+++ client/src/postcss.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ]
-}
+    require("tailwindcss"),
+    require("autoprefixer"),
+    require("@fullhuman/postcss-purgecss")({
+      content: ["../public/*.html", "../public/*.js", "./build/*.css"],
+      defaultExtractor: (content) => content.match(/[A-Za-z0-9-_:/]+/g) || [],
+    }),
+  ],
+};
--- client/src/tailwind.config.js
+++ client/src/tailwind.config.js
@@ -1,34 +1,38 @@
 module.exports = {
+  purge: ["../public/*.html", "../public/*.js", "./build/*.css"],
   theme: {
     zIndex: {
-      '-10': '-10',
+      "-10": "-10",
     },
     screens: {
-      sm: '576px',
-      md: '768px',
-      lg: '992px',
-      xl: '1200px',
+      xs: "375px",
+      sm: "576px",
+      md: "768px",
+      lg: "992px",
+      xl: "1200px",
     },
     fontFamily: {
-      display: ['Gilroy', 'sans-serif'],
-      body: ['Graphik', 'sans-serif'],
+      display: ["Gilroy", "sans-serif"],
+      body: ["Graphik", "sans-serif"],
     },
     borderWidth: {
-      default: '1px',
-      '0': '0',
-      '2': '2px',
-      '4': '4px',
+      default: "1px",
+      "0": "0",
+      "2": "2px",
+      "4": "4px",
     },
     extend: {
       colors: {
-        cyan: '#9cdbff',
+        cyan: "#9cdbff",
       },
       spacing: {
-        '96': '24rem',
-        '128': '32rem',
-      }
-    }
+        "64": "16rem",
+        "72": "18rem",
+        "96": "24rem",
+        "128": "32rem",
+      },
+    },
   },
   variants: {},
   plugins: [],
-}
+};
