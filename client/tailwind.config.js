e8476d530e16dd2f06bdb4ec8e993ca9cce68554
Yggdrasil <gregselvagem@gmail.com>
2020-08-23 15:33:28 -0400

    -postcss on client scripts

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-  purge: ["./src/**/**/**/*.js", "./public/*.css", "./public/*.html"],
+  purge: {
+    enabled: true,
+    content: ["./src/**/**/**/*.js", "./public/*.css", "./public/*.html"],
+  },
   target: "relaxed",
   prefix: "",
   important: false,
