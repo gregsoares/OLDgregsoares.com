137bbfcfa53f73053b55c2a029f8efc8fdeca8bb
Yggdrasil <gregselvagem@gmail.com>
2020-09-06 13:20:14 -0400

    +PostSection style&content, +MernTemp&Insure Post

--- client/src/Components/PostSection/PostSection.js
+++ client/src/Components/PostSection/PostSection.js
@@ -1,44 +1,15 @@
 import React from "react";
+import { MernTemplate } from "./MernTemplate";
+import { InsureChallenge } from "./InsureChallenge";
 
-// ==> props <==
-// postTitle
-// postText
-// postImg
-
-export const PostSection = (props) => {
-  const { postImg, postTitle, postText } = props.content;
+export const PostSection = () => {
   return (
     <div
       className="px-4 py-8 font-sans text-gray-500 "
       data-testid="PostSectionContainer"
     >
-      <div className="max-w-2xl mx-auto overflow-hidden rounded-md shadow-md brand-lighterBlue">
-        <div className="flex flex-col items-center justify-center leading-normal tracking-wide text-center ">
-          <img
-            src={postImg}
-            alt="PlaceHolder"
-            className="object-cover w-full"
-          />
-        </div>
-
-        <div className="py-8 mx-8 ">
-          <h3 className="my-6 text-2xl text-center text-gray-400 sm:text-3xl ">
-            {postTitle}
-          </h3>
-          <p className="mx-1 leading-relaxed tracking-wide sm:mx-2 md:mx-4">
-            {postText}
-          </p>
-        </div>
-
-        {/* Footer section */}
-        <div className="mt-2 text-center text-grey-darker">
-          <h3 className="mb-4 text-base sm:text-lg">
-            <button className="w-full py-4 my-8 text-sm tracking-wide text-gray-300 border rounded shadow-md brand-blue-gradient hover:text-white hover:shadow-lg">
-              Live Preview
-            </button>
-          </h3>
-        </div>
-      </div>
+      <InsureChallenge />
+      <MernTemplate />
     </div>
   );
 };
--- client/src/Pages/Projects.js
+++ client/src/Pages/Projects.js
@@ -2,31 +2,11 @@ import React from "react";
 
 // Project Components import
 import { PostSection } from "../Components/PostSection/PostSection";
-import { MessagesPanel } from "../Components/MessagesPanel/MessagesPanel";
 
-const insureChallenge = [
-  {
-    postTitle: "Lorem Ipsum Morel ",
-    postText:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque doloribus, obcaecati aut temporibus incidunt officia, voluptatum minus debitis consequatur architecto suscipit non nesciunt placeat quisquam beatae? Rem, dolore! Qui, aliquam. ",
-    postImg: "https://placeimg.com/640/480/any",
-  },
-  {
-    postTitle: "Title",
-    postText:
-      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloremque doloribus, obcaecati aut temporibus incidunt officia, voluptatum minus debitis consequatur architecto suscipit non nesciunt placeat quisquam beatae? Rem, dolore! Qui, aliquam. ",
-    postImg: "https://placeimg.com/640/480/any",
-  },
-];
 const Projects = () => {
   return (
     <div className="items-center justify-center brand-darkBlue">
-      <PostSection content={insureChallenge[0]} />
-      <PostSection content={insureChallenge[1]} />
-      <PostSection content={insureChallenge[0]} />
-      <PostSection content={insureChallenge[1]} />
-      <PostSection content={insureChallenge[0]} />
-      <PostSection content={insureChallenge[1]} />
+      <PostSection />
     </div>
   );
 };
